7d90d229097056766b3182bd971787c600670ab8
Han Frisonsova <hanafrisonsova@gmail.com>
2025-03-29 16:50:43 +0100

    prida pozdrav na konec emailu

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const goodbye = (fullName) => {
+  const closing = document.querySelector('.email__closing'); //pozor na .class
+  closing.innerHTML = `S pozdravem ${fullName}`;
+};
 
+const yourName = 'Jan Novák'; //nejdriv definuji tu hodnotu
+const addName = goodbye(yourName); //a pak volám funkci
